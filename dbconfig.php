6ff18797895ffdf3bed1e3c3c84f5b34f72db1e3
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-04-23 15:59:43 +0300

    fixed the db error

--- dbconfig.php
+++ dbconfig.php
@@ -1,10 +1,33 @@
 <?php
 
+// session_start();
+// DEFINE("server","localhost",true);
+// DEFINE("user","root",true);
+// DEFINE("password","",true);
+// DEFINE("database","online_grocery",true);
+// 	function iud($query)
+// 	{
+// 		$cid=mysqli_connect(server,user,password,database) or die("connection error");
+// 	$result=mysqli_query($cid,$query);
+// 	$n=mysqli_affected_rows($cid);
+// 	mysqli_close($cid);
+// 	return $n;
+// 	}
+	
+// function select($query)
+// {
+// 	$cid=mysqli_connect(server,user,password,database) or die("connection error");
+// 	$result=mysqli_query($cid,$query);
+// 	mysqli_close($cid);
+// 	return $result;
+// }
+
+// Get Heroku ClearDB connection information
 session_start();
-DEFINE("server","localhost",true);
-DEFINE("user","root",true);
-DEFINE("password","",true);
-DEFINE("database","online_grocery",true);
+DEFINE("server","us-cdbr-east-05.cleardb.net",true);
+DEFINE("user","bcb37cccb8ecad",true);
+DEFINE("password","6b9a18b4",true);
+DEFINE("database","heroku_0b1fd23f5c433ca",true);
 	function iud($query)
 	{
 		$cid=mysqli_connect(server,user,password,database) or die("connection error");
@@ -21,14 +44,5 @@ function select($query)
 	mysqli_close($cid);
 	return $result;
 }
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 ?>
